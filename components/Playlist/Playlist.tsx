692cc3fecf927b1818c959c0829c97ac1d57e073
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-02-11 23:16:53 -0500

    updates

---
+++ components/Playlist/Playlist.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useContext, createRef } from 'react'; 
+import { Button, Text, View } from 'react-native';
+import { Input, Overlay } from 'react-native-elements';
+import { PlaylistContext } from '../providers/Spotify';
+import Song from './Song';
+
+
+const Playlist: React.FC<{}> = () => {
+    const {createPlaylist, clearPlaylist, currentPlaylist} = useContext(PlaylistContext);
+    return(
+        <View>
+            {
+                currentPlaylist && 
+                currentPlaylist.tracks && 
+                currentPlaylist.tracks.items.map(item => {
+                    const {track} = item;
+                    return(
+                        <Song song={track} />
+                    )})
+            }
+        </View>
+    )
+}
+
+// tslint:disable-next-line: interface-name
+interface CreatePlaylistOverlayProps {
+    visible: boolean,
+    setVisible: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const CreatePlaylistOverlay: React.FC<CreatePlaylistOverlayProps> = ({visible, setVisible}) => {
+    const { createPlaylist } = useContext(PlaylistContext);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+
+    const inputName = createRef<Input>();
+    const inputDescription = createRef<Input>();
+
+    const onSubmit = () => {
+        createPlaylist({name, description, public: false, collaborative: false});
+        setVisible(false);
+    }
+
+    return(
+        <Overlay isVisible={visible}>
+            <>
+                <Input ref={inputName} placeholder="Name" label="Name" />
+                <Input ref={inputDescription} placeholder="Description" label="Description"/>
+                <Button onPress={onSubmit} title="Submit"/>
+                <Button onPress={() => setVisible(false)} title="Clear" />
+            </>
+        </Overlay>
+        
+    )
+}
+
+export {Playlist, CreatePlaylistOverlay};
